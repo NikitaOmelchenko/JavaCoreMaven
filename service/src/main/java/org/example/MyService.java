72bddba64b9631f4a01decd199baa358f2b8f8d7
NikitaOmelchenko <nikeone463@gmail.com>
2023-03-02 22:38:16 +0200

    Maven

---
+++ service/src/main/java/org/example/MyService.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import Unknown.Db;
+import Unknown.DbSetting;
+import Unknown.MyEntity;
+
+import java.util.UUID;
+
+public class MyService {
+
+    private DbSetting dbSetting = new DbSetting("name", "password");
+    private String name = "myService";
+    private Db db = new Db(dbSetting);
+
+    public String getName() {
+        return name;
+    }
+
+    public MyEntity setMyEntity(MyEntity myEntity) {
+        myEntity.setId(UUID.randomUUID());
+        db.setMyEntity(myEntity);
+        return myEntity;
+    }
+
+    public MyEntity getMyEntity() {
+        return db.getMyEntity();
+    }
+}
